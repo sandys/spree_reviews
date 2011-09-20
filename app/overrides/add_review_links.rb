36e02ec76712fb1b077232c5bbeb7a9578acac9a
Sandeep Srinivasa <sandys@gmail.com>
2011-09-21 00:29:00 +0530

    updated hooks to show buttons and menu items

--- app/overrides/add_review_links.rb
+++ app/overrides/add_review_links.rb
@@ -1,12 +1,13 @@
 
 Deface::Override.new(:virtual_path => "layouts/admin",
                      :name => "review_management",
-                     :insert_bottom => "[data-hook='admin-tabs']",
-                     :partial => "admin/reviews"
-                    )
+#                     :insert_bottom => "[data-hook='admin-tabs']",
+                     :insert_bottom => "div#admin-menu ul",
+                     :partial => "admin/hooks/reviews_tab",
+                     :disabled => false)
 
 Deface::Override.new(:virtual_path => "admin/configurations/index",
                      :name => "review_settings",
-                     :insert_bottom => "[data-hook='admin-tabs']",
-                     :partial => "admin/reviews"
-                    )
+                     :insert_bottom => "[data-hook='admin_configurations_menu']",
+                     :partial => "admin/hooks/reviews_settings",
+                     :disabled => false)
